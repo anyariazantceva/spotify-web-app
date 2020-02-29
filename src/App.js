76128394239fae97a1b6492cf9f4b6ab7c111a3a
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-29 21:01:24 +0200

    Set up auth and spotify api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import AppHeader from "./components/AppHeader";
-import Footer from "./components/Footer";
-import SearchBar from "./components/SearchBar";
-import ArtistsList from "./components/ArtistsList";
 
+import SpotifyWebApi from 'spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
 
+class App extends Component {
+    constructor(){
+        super();
+        const params = this.getHashParams();
+        const token = params.access_token;
+        if (token) {
+            spotifyApi.setAccessToken(token);
+        }
+        this.state = {
+            loggedIn: token ? true : false,
+            nowPlaying: { name: 'Not Checked', albumArt: '' },
+            artist: {name: '', image: ''}
+        }
+    }
+    getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        e = r.exec(q)
+        while (e) {
+            hashParams[e[1]] = decodeURIComponent(e[2]);
+            e = r.exec(q);
+        }
+        return hashParams;
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <AppHeader></AppHeader>
-      <SearchBar/>
-      <ArtistsList/>
-      <Footer/>
-    </div>
-  );
+    getNowPlaying(){
+        spotifyApi.getMyCurrentPlaybackState()
+            .then((response) => {
+                this.setState({
+                    nowPlaying: {
+                        name: response.item.name,
+                        albumArt: response.item.album.images[0].url
+                    }
+                });
+            })
+    }
+
+    getArtist = () => {
+        spotifyApi.searchArtists('Loathe')
+            .then((response) => {
+                this.setState({
+                    artist: {
+                        name: response.artists.items[0].name,
+                        image: response.artists.items[0].images[0].url
+                    }
+                })
+                console.log(this.state.artist)
+        })
+    }
+    render() {
+        return (
+            <div className="App">
+                <a href='http://localhost:8888' > Login to Spotify </a>
+                <div>
+                    Now Playing: { this.state.nowPlaying.name }
+                </div>
+                <div>
+                    <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
+                </div>
+                { this.state.loggedIn &&
+                <button onClick={() => this.getNowPlaying()}>
+                    Check Now Playing
+                </button>
+                }
+                <button onClick={this.getArtist}>Search</button>
+                <div>Artist name {this.state.artist.name}</div>
+                <div>Artist image <img src={this.state.artist.image} alt=""/></div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/ArtistsList.js
+++ src/components/ArtistsList.js
@@ -1,63 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './ArtistsList.css';
 
 
-const artists = [
-    {
-        name: 'Architects',
-        vocalist:  'Sam Carter',
-        genre: 'metalcore',
-        id: '1',
-        image: './13592.jpg'
-    },
-    {
-        name: 'Counterparts',
-        vocalist: 'Brendan Murphy',
-        genre: 'hardcore, metalcore',
-        id: '2',
-        image: './counteparts.jpg'
-    },
-    {
-        name: 'Northlane',
-        vocalist: 'Marcus Bridge',
-        genre: 'metalcore',
-        id: '3',
-        image: './1200px-Northlane_2019_gen_use_lores.jpg'
-    },
-    {
-        name: 'Loathe',
-        vocalist: "Kadeem France",
-        genre: 'experimental',
-        id: '4',
-        image: './loathe-band-promo-171.jpg'
-    },
-    {
-        name: 'Tesseract',
-        vocalist: 'Daniel Thompson',
-        genre: 'progressive',
-        id: '5',
-        image: './unnamed.jpg'
+export default class ArtistsList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchValue: '',
+            artists: null
+        }
     }
-];
 
-const listItems = artists.map((artist) => {
-    return <div key={artist.id} className="artists__item">
-            <div className="artist__name">{artist.name}</div>
-            <div className="artist__genre">{artist.genre}</div>
-            <div className="artist__vocalist">{artist.vocalist}</div>
-            <div className="artist__image">
-                <img src={artist.image} alt=""/>
+    handleChange = (e) => {
+        this.setState({
+            searchValue: e.target.value
+        })
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({
+            searchValue: e.target.value
+        })
+        fetch('https://api.spotify.com/v1/me/player/recently-played')
+            .then(response => response.json())
+            .then(data => console.log(data))
+
+    };
+
+
+    render() {
+        return (
+            <div className='container'>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleChange} value={this.state.value}/>
+                    <button>Search</button>
+                </form>
+                <div className="list">
+
+                </div>
             </div>
-
-        </div>
-});
-
-const ArtistsList = () => {
-    return (
-        <div className="artists__list">
-            {listItems}
-        </div>
-    )
+        )
+    }
 }
-
-export default ArtistsList
